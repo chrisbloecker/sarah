c98730c20dba986110ce12cb4494b70fafd47450
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-09-27 17:56:54 +0800

    added project for frontend in elm and updated licenses

---
+++ src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (main)
+
+import Html                   exposing (..)
+import Html.App        as App
+import Html.Attributes        exposing (..)
+import Html.Events            exposing (onClick)
+import Http
+import Json.Decode as Json
+import Task
+
+
+main = App.program { init          = init "dogs"
+                   , view          = view
+                   , update        = update
+                   , subscriptions = subscriptions
+                   }
+
+
+-- Model
+
+type alias Model = { topic  : String
+                   , gifUrl : String
+                   }
+
+init : String -> (Model, Cmd Msg)
+init topic = ( Model topic "waiting.gif"
+             , getRandomGif topic
+             )
+
+
+-- Update
+
+type Msg = MorePlease
+         | FetchSucceed String
+         | FetchFail    Http.Error
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease          -> (model,                    getRandomGif model.topic)
+    FetchSucceed newUrl -> (Model model.topic newUrl, Cmd.none)
+    FetchFail _         -> (model, Cmd.none)
+
+-- View
+
+view : Model -> Html Msg
+view model =
+  div []
+      [ h2 [] [ text model.topic ]
+      , img [ src model.gifUrl ] []
+      , button [ onClick MorePlease ] [ text "More Please!" ]
+      ]
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
+-- Http
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
+
+decodeGifUrl : Json.Decoder String
+decodeGifUrl = Json.at ["data", "image_url"] Json.string
