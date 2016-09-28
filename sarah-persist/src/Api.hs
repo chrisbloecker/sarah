71f6821e3547ea013fa22cec111f51b512e47cb0
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-09-28 13:17:29 +0800

    added own cors

--- sarah-persist/src/Api.hs
+++ sarah-persist/src/Api.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators     #-}
 --------------------------------------------------------------------------------
 module Api
@@ -10,8 +11,8 @@ import Control.Monad.Reader        (runReaderT)
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Text                   (Text)
-import Network.Wai                 (Application)
-import Network.Wai.Middleware.Cors (simpleCors)
+import Network.Wai                 (Application, Middleware)
+import Network.Wai.Middleware.Cors (CorsResourcePolicy (..), cors, simpleCorsResourcePolicy)
 import Servant
 --------------------------------------------------------------------------------
 import Api.Sensor
@@ -23,7 +24,7 @@ type Api = SensorApi :<|> Raw
 --------------------------------------------------------------------------------
 
 sensorApp :: Config -> Application
-sensorApp config = simpleCors (serve (Proxy :: Proxy SensorApi) (appToServer config))
+sensorApp config = myCors (serve (Proxy :: Proxy SensorApi) (appToServer config))
 
 appToServer :: Config -> Server SensorApi
 appToServer config = enter (convertApp config) sensorServer
@@ -36,3 +37,8 @@ files = serveDirectory "assets"
 
 app :: Config -> Application
 app config = serve (Proxy :: Proxy Api) (appToServer config :<|> files)
+
+myCors :: Middleware
+myCors = cors (const $ Just policy)
+  where
+    policy = simpleCorsResourcePolicy { corsRequestHeaders = ["Content-Type"] }
