971653dd71149b39aa7f2ee1e5e8712c53b41cda
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-09-30 10:13:48 +0800

    factoring into separate repositories

---
+++ sarah-persist/src/Api.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE TypeOperators     #-}
+--------------------------------------------------------------------------------
+module Api
+  ( app
+  ) where
+--------------------------------------------------------------------------------
+import Control.Monad.Except        (ExceptT)
+import Control.Monad.Reader        (runReaderT)
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Text                   (Text)
+import Network.Wai                 (Application)
+import Servant
+--------------------------------------------------------------------------------
+import Api.Sensor
+import Config
+--------------------------------------------------------------------------------
+
+type Api = SensorApi :<|> Raw
+
+--------------------------------------------------------------------------------
+
+sensorApp :: Config -> Application
+sensorApp config = serve (Proxy :: Proxy SensorApi) (appToServer config)
+
+appToServer :: Config -> Server SensorApi
+appToServer config = enter (convertApp config) sensorServer
+
+convertApp :: Config -> AppM :~> ExceptT ServantErr IO
+convertApp config = Nat (flip runReaderT config . runApp)
+
+files :: Application
+files = serveDirectory "assets"
+
+app :: Config -> Application
+app config = serve (Proxy :: Proxy Api) (appToServer config :<|> files)
