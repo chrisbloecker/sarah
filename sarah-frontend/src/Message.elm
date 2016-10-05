d498f3b489ca271e2bd479880f913ef75135c5bc
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-10-05 17:40:05 +0800

    back to submodules

--- sarah-frontend/src/Message.elm
+++
@@ -1,9 +0,0 @@
-module Message
-  exposing (Message (..))
---------------------------------------------------------------------------------
-import Home.Message    as Home
-import Sensors.Message as Sensors
---------------------------------------------------------------------------------
-
-type Message = HomeMessage    Home.Message
-             | SensorsMessage Sensors.Message
