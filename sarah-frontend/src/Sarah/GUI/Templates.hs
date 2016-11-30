b9d660088adec3e3d028340f8705cea5864a8a65
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-11-30 22:41:48 +0800

    making sense of blaze

--- sarah-frontend/src/Sarah/GUI/Templates.hs
+++ sarah-frontend/src/Sarah/GUI/Templates.hs
@@ -1,3 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+--------------------------------------------------------------------------------
 module Sarah.GUI.Templates
   where
 --------------------------------------------------------------------------------
+import           Prelude                     hiding (div, span, id)
+import           Text.Blaze.Html5
+import           Text.Blaze.Html5.Attributes hiding (span)
+--------------------------------------------------------------------------------
+--import qualified Text.Blaze.Html5 as H
+--------------------------------------------------------------------------------
+
+mkNavbar :: Html
+mkNavbar = nav ! class_ "navbar navbar-default" $
+             div ! class_ "container-fluid" $ do
+               div ! class_ "navbar-header" $
+                 button ! type_ "button"
+                        ! class_ "navbar-toggle collapsed"
+                        ! dataAttribute "target" "the-navbar"
+                        ! customAttribute "aria-expanded" "false" $ do
+                   span ! class_ "icon-bar" $ ""
+                   span ! class_ "icon-bar" $ ""
+                   span ! class_ "icon-bar" $ ""
+               div ! class_ "collapse navbar-collapse"
+                   ! id "the-navbar"
