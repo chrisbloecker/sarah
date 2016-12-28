b553ac9ddaeee303505647c6447c96fa673afeab
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-12-28 20:14:01 +0800

    extras for threepenny

---
+++ sarah-frontend/src/Graphics/UI/Threepenny/Extra.hs
@@ -0,0 +1,12 @@
+module Graphics.UI.Threepenny.Extra
+  where
+--------------------------------------------------------------------------------
+import Graphics.UI.Threepenny
+--------------------------------------------------------------------------------
+
+nav          = mkElement "nav"
+ariaexpanded = attr "aria-expanded"
+ariahaspopup = attr "aria-haspopup"
+datatoggle   = attr "data-toggle"
+datatarget   = attr "data-target"
+role         = attr "role"
