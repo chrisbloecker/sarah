ea866c36d501a35a17c4494c5b42c57021354110
Christopher Blöcker <chrisbloecker@googlemail.com>
2016-12-29 17:31:16 +0800

    don't read the sensor at start anymore...

--- sarah-middleware/app/Main.hs
+++ sarah-middleware/app/Main.hs
@@ -26,8 +26,6 @@ import           Sarah.Middleware.Slave
 --------------------------------------------------------------------------------
 import qualified Data.ByteString as BS
 import qualified Data.Yaml       as Y
-import qualified Sarah.Middleware.Device.Sensor.DHT22 as DHT22
-import           Raspberry.GPIO
 --------------------------------------------------------------------------------
 
 data NodeRole = RoleMaster | RoleSlave
@@ -55,7 +53,6 @@ corsPolicy = cors (const $ Just policy)
 go :: Options -> IO ()
 go Options{..} = case nodeRole of
   RoleMaster -> do
-    print =<< DHT22.get (Pin 4)
     let masterSettingsFile = fromMaybe "master.yml" settingsFile
     mSettings <- Y.decodeEither <$> BS.readFile masterSettingsFile
 
