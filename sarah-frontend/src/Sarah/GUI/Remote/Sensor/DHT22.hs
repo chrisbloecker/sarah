c99e3a47ffb04d0aebeac86344e1a9b9099f8d54
Christopher Blöcker <chrisbloecker@googlemail.com>
2017-03-09 00:00:38 +0800

    added labels to DHT22 remote to show temperature and humidity

--- sarah-frontend/src/Sarah/GUI/Remote/Sensor/DHT22.hs
+++ sarah-frontend/src/Sarah/GUI/Remote/Sensor/DHT22.hs
@@ -1,30 +1,41 @@
 module Sarah.GUI.Remote.Sensor.DHT22
   where
-
+--------------------------------------------------------------------------------
 import Graphics.UI.Threepenny  hiding (map)
 import Prelude                 hiding (span, div)
-import Sarah.GUI.Model                (HasRemote (..), sendCommand, embedUI)
+import Physics
+import Sarah.GUI.Model                (HasRemote (..), sendCommand, embedUI, handleResponse, doNothing)
+import Sarah.GUI.Widgets
 import Sarah.Middleware               (mkCommand)
 import Sarah.Middleware.Device        (DHT22)
+--------------------------------------------------------------------------------
 import qualified Sarah.Middleware.Device.Sensor.DHT22 as DHT22
+--------------------------------------------------------------------------------
 
 instance HasRemote DHT22 where
   renderRemote appEnv deviceAddress _ = do
-    readTemperatureButton <- button # set class_ "btn btn-sm btn-default" #+ [ span # set class_ "fa fa-thermometer-full" ]
-    readHumidityButton    <- button # set class_ "btn btn-sm btn-default" #+ [ span # set class_ "glyphicon glyphicon-tint" ]
+    (eventTemperature, handlerTemperature) <- liftIO newEvent
+    (eventHumidity,    handlerHumidity)    <- liftIO newEvent
 
-    on click readTemperatureButton $ embedUI $ do
+    behaviourTemperature <- stepper "--°C" eventTemperature
+    behaviourHumidity    <- stepper "--%"  eventHumidity
+
+    temperatureDisplay <- reactiveLabel behaviourTemperature
+    humidityDisplay    <- reactiveLabel behaviourHumidity
+
+    getTemperatureButton <- button # set class_ "btn btn-sm btn-default" #+ [ span # set class_ "fa fa-thermometer-full" ]
+    getHumidityButton    <- button # set class_ "btn btn-sm btn-default" #+ [ span # set class_ "glyphicon glyphicon-tint" ]
+
+    on click getTemperatureButton $ embedUI $ do
       mres <- sendCommand appEnv deviceAddress (mkCommand DHT22.GetTemperature)
-      case mres of
-        Nothing  -> putStrLn "[DHT22.readTemperatureButton.click] No response"
-        Just res -> putStrLn "[DHT22.readTemperatureButton.click] Got response"
+      handleResponse "[DHT22.getTemperatureButton.click]" mres doNothing (\(Temperature t) -> handlerTemperature $ show t ++ "°C")
 
-    on click readHumidityButton $ embedUI $ do
+    on click getHumidityButton $ embedUI $ do
       mres <- sendCommand appEnv deviceAddress (mkCommand DHT22.GetHumidity)
-      case mres of
-        Nothing  -> putStrLn "[DHT22.readHumidityButton.click] No response"
-        Just res -> putStrLn "[DHT22.readHumidityButton.click] Got response"
+      handleResponse "[DHT22.getHumidityButton.click]" mres doNothing (\(Humidity h) -> handlerHumidity $ show h ++ "%")
 
     div #+ [ p # set class_ "text-center"
-               #+ map element [ readTemperatureButton, readHumidityButton ]
+               #+ [ element temperatureDisplay, element getTemperatureButton ]
+           , p # set class_ "text-center"
+               #+ [ element humidityDisplay, element getHumidityButton ]
            ]
