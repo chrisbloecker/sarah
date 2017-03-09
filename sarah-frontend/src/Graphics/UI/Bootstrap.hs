b2bc7b4dfc67d63308cb41f44ab5b7fb4a296e2d
Christopher Blöcker <chrisbloecker@googlemail.com>
2017-03-09 19:40:34 +0800

    forgot to check in bootstrap file

---
+++ sarah-frontend/src/Graphics/UI/Bootstrap.hs
@@ -0,0 +1,8 @@
+module Graphics.UI.Bootstrap
+  ( module Graphics.UI.Bootstrap
+  ) where
+
+import Graphics.UI.Bootstrap.Builder   as Graphics.UI.Bootstrap
+import Graphics.UI.Bootstrap.Class     as Graphics.UI.Bootstrap
+import Graphics.UI.Bootstrap.Glyphicon as Graphics.UI.Bootstrap
+import Graphics.UI.Bootstrap.Style     as Graphics.UI.Bootstrap
